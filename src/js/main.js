ce1b491dbf6540dae53cadf29d0bee9307bddbc9
YevheniiBrodovskyii <zhenyabroddd@gmail.com>
2022-03-28 23:25:53 +0200

    Add over pictures

--- src/js/main.js
+++ src/js/main.js
@@ -7,6 +7,7 @@ import showMoreStyles from "./modules/showMoreStyles";
 import calc from "./modules/calc";
 import changeModalState from "./modules/changeModalState";
 import filter from "./modules/filter";
+import pictureSize from "./modules/pictureSize";
 
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
@@ -24,5 +25,7 @@ window.addEventListener('DOMContentLoaded', () => {
     showMoreStyles('.button-styles', '#styles .row');
     calc('#size', '#material', '#options', '.promocode', '.calc-price');
     filter();
+    pictureSize('.sizes-block');
+
 
 });
